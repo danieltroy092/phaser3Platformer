c53291e31dbadaac1e8940f51906bcef688c79c6
Daniel Troy Ong <danieltroy092@gmail.com>
2020-06-09 18:41:16 +0100

    Loaded sprites into canvas then attached animation onto sprite

--- src/scenes/Game.js
+++ src/scenes/Game.js
@@ -7,11 +7,25 @@ class Game extends Phaser.Scene {
   }
 
   preload() {
-    this.load.image('logo', 'assets/phaser3-logo.png');
+    this.load.spritesheet('hero-run-sheet', 'assets/hero/run.png', {
+      frameWidth: 32,
+      frameHeight: 64,
+    });
   }
 
   create(data) {
-    this.add.image(400, 300, 'logo');
+    this.anims.create({
+      key: 'hero-running',
+      frames: this.anims.generateFrameNumbers('hero-run-sheet'),
+      frameRate: 10, //10th of a second
+      repeat: -1,
+    });
+
+    //Load sprite onto canvas
+    this.player = this.add.sprite(400, 300, 'hero-run-sheet');
+
+    //assign animation to sprite
+    this.player.anims.play('hero-running');
   }
 
   update(time, delta) {}
