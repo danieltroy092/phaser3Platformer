2f30baeceb86718d81274874017bba3b8c6624dd
Daniel Troy Ong <danieltroy092@gmail.com>
2020-06-10 20:01:47 +0100

    Creating animations and loaded spritesheets for said animations

--- src/scenes/Game.js
+++ src/scenes/Game.js
@@ -7,8 +7,34 @@ class Game extends Phaser.Scene {
     super({ key: 'GameScene' });
   }
 
+  // loading spritesheet images
   preload() {
+    this.load.spritesheet('hero-idle-sheet', 'assets/hero/idle.png', {
+      frameWidth: 32,
+      frameHeight: 64,
+    });
+
     this.load.spritesheet('hero-run-sheet', 'assets/hero/run.png', {
+      frameWidth: 32,
+      frameHeight: 64,
+    });
+
+    this.load.spritesheet('hero-pivot-sheet', 'assets/hero/pivot.png', {
+      frameWidth: 32,
+      frameHeight: 64,
+    });
+
+    this.load.spritesheet('hero-jump-sheet', 'assets/hero/jump.png', {
+      frameWidth: 32,
+      frameHeight: 64,
+    });
+
+    this.load.spritesheet('hero-flip-sheet', 'assets/hero/spinjump.png', {
+      frameWidth: 32,
+      frameHeight: 64,
+    });
+
+    this.load.spritesheet('hero-fall-sheet', 'assets/hero/fall.png', {
       frameWidth: 32,
       frameHeight: 64,
     });
@@ -18,12 +44,43 @@ class Game extends Phaser.Scene {
     // capturing keyboard input
     this.cursorKeys = this.input.keyboard.createCursorKeys();
 
-    // initialises animation for sprite
+    // initialises animation for sprites
     this.anims.create({
-      key: 'hero-running',
+      key: 'hero-idle', // idle animation
+      frames: this.anims.generateFrameNumbers('hero-idle-sheet'),
+    });
+
+    this.anims.create({
+      key: 'hero-running', // running animation
       frames: this.anims.generateFrameNumbers('hero-run-sheet'),
-      frameRate: 10, //10th of a second
-      repeat: -1, //-1 to run inifinitely
+      frameRate: 10, // 10fps
+      repeat: -1, // -1 to run infinitely
+    });
+
+    this.anims.create({
+      key: 'hero-pivoting', // pivoting animation
+      frames: this.anims.generateFrameNumbers('hero-pivot-sheet'),
+    });
+
+    this.anims.create({
+      key: 'hero-jumping', // jumping animation
+      frames: this.anims.generateFrameNumbers('hero-jump-sheet'),
+      frameRate: 10, // 10fps
+      repeat: -1, // -1 to run infinitely
+    });
+
+    this.anims.create({
+      key: 'hero-flipping', // flipping animation
+      frames: this.anims.generateFrameNumbers('hero-flip-sheet'),
+      frameRate: 30, // 30fps
+      repeat: 0,
+    });
+
+    this.anims.create({
+      key: 'hero-falling',
+      frames: this.anims.generateFrameNumbers('hero-fall-sheet'),
+      frameRate: 10, // 10fps
+      repeat: -1, // -1 to run infinitely
     });
 
     // creates a new hero on screen
