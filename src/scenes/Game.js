a496b7fb41bcff281ae3903a59c7507dd2e193bc
Daniel Troy Ong <danieltroy092@gmail.com>
2020-06-13 18:23:07 +0100

    Added physics for the sprite to collide with certain tiles within game level

--- src/scenes/Game.js
+++ src/scenes/Game.js
@@ -92,9 +92,8 @@ class Game extends Phaser.Scene {
     // Loads game level
     this.addMap();
 
-    // creates a new hero on screen
-    // positioned in centre with hardcoded values
-    this.hero = new Hero(this, 250, 160);
+    // Loads hero
+    this.addHero();
 
     // camera manager
     // Set camera to stay within bounds
@@ -109,15 +108,28 @@ class Game extends Phaser.Scene {
     this.cameras.main.startFollow(this.hero);
   }
 
+  // creates a new hero on screen
+  addHero() {
+    // positioned in centre with hardcoded values
+    this.hero = new Hero(this, 250, 160);
+
+    // add a collider physics for the hero and 'Ground' layer.
+    this.physics.add.collider(
+      this.hero,
+      this.map.getLayer('Ground').tilemapLayer
+    );
+  }
+
+  // loads map
   addMap() {
-    // loads map
     this.map = this.make.tilemap({ key: 'level-1' });
 
     // add tileset image
     const groundTiles = this.map.addTilesetImage('world-1', 'world-1-sheet');
 
-    // create the layer
-    this.map.createStaticLayer('Ground', groundTiles);
+    // create layer
+    const groundLayer = this.map.createStaticLayer('Ground', groundTiles);
+    groundLayer.setCollision([1, 2, 4], true); // set which tiles to collide with
 
     // set tilemap size in-line with browser screen size
     this.physics.world.setBounds(
